4e8ff141fdf971b249e69973a3eed08b799f3035
Mark Murnane <MMURNANE@ie.ibm.com>
2018-09-24 23:35:38 +0100

    Last updates

--- Etivity2_ID18195327_Reflection.py
+++ Etivity2_ID18195327_Reflection.py
@@ -1,11 +1,5 @@
-# **Student Name: Mark Murnane **
-# 
-# **Student ID: 18195326 **
-
-# First part is to build a function that can parse an equation term for us
-
 import re
-
+import unittest
 
 def is_number(value):
      
@@ -50,20 +44,8 @@ def number_to_string (value):
              
     return number_string
 
-# Need to take in text and parse it into a tuple list
-# No need for a differentiate term function : use a lamba to call f'(term)
-# Similar for parse funciton
-# Text is handled by a print function on top of the parse function
-#    Consider using the console pretty print capabilities highlighted by Fergus?
-# Test assertions
-# Test class with polynomials from question
-# Test class with edge cases such as f'(x) where there is no x, x^1 and combination terms
-
 def parse_term (equation_term, differential):
     """Parses a string with an algebraic term (e.g. 5x^2) and returns the coefficient and exponent parts of the term.
-    
-    If multiple variables are present in the term, and they have exponents, they should be separated using *.  This
-    allows for multiple exponents to be present in the term.
     
     Args: 
         equation_term (str):    A string with an algebraic term (e.g. 5x^2)
@@ -72,28 +54,16 @@ def parse_term (equation_term, differential):
     Returns:
         (tuple)                 The coefficient and exponent of the<differential part of the term
     """
-    
-    # Need to return (coefficient, power) and then expand to include ([coefficients], power)
-    # May need to recurse in here to handle any other variable coefficients
-    
-    # Do the simple case first
-        
-    # Split the equation_term on the differential
-    # Then have coefficient and power
-    
+   
     if equation_term == '' or differential == '':
         return None
     
+    components = equation_term.split(differential)      
     coefficient = exponent = 0
-    
-    
-    # Simple case is             
+      
     if differential not in equation_term:
         coefficient = float(equation_term)
     else:
-        # Here instead of splitting on the differential, could split on the exponent value
-        # May need to consider using maxsplit == 1 (differential should only appear once) and then parse term inside this
-        
         components = equation_term.split(differential)              
         
         # Left part is the coefficient
@@ -110,134 +80,60 @@ def parse_term (equation_term, differential):
     
     return (coefficient, exponent)
 
-print ("Parsing tests ...")
-
-print(parse_term("16x", 'x'))
-print(parse_term("3x^2", 'x'))
-print(parse_term('x', 'x'))
-print(parse_term('12', 'x'))
-print(parse_term('', 'x'))
-
-#print(parse_term("ax^3", 'x')
-
-
 def print_term(equation_term, differential):
+
+    """Formats a polynomial algebraic term from it's tuple representation into a string.
+    
+    Args:
+        equation_term        Tuple containing the (coefficient, exponent) of the term.
+        differential (str)   The variable in the polynomial term.
+        
+    Returns:   
+        (str)                The formatted string <coefficient><differential>^<exponent>."""
   
-    if equation_term == "" or equation_term == None:
-        return ''
-    elif differential == "" or equation_term == None:
+    if equation_term == None or differential == None:
         return ''
     
-    coefficient, exponent = equation_term    
-    
+    coefficient, exponent = equation_term        # Unpacks the tuple
     diff_term = ''
-    coefficient_str = number_to_string(coefficient)
     
+    coefficient_str = number_to_string(coefficient)
+
+    # If there is no exponent, then just print the coefficient.  O
+    # Otherwise format the string with the coefficient and exponent (both only if > 1)    
     if exponent == 0:
         diff_term = "{0}".format(coefficient_str)
     else:
-        if coefficient == 1:
-            coefficient_str = ''
+        diff_term = str(differential)        
+        if coefficient > 1:
+            diff_term = "{0}{1}".format(coefficient_str, diff_term)
             
-        if exponent == 1:
-            diff_term = "{0}{1}".format(coefficient_str, differential)
-        else:
+        if exponent > 1:
             diff_term = "{0}{1}^{2}".format(coefficient_str, differential, exponent)
     
     return diff_term
 
-print(print_term(parse_term("16x", 'x'), 'x'))
-print(print_term(parse_term("3x^2", 'x'), 'x'))
-print(print_term(parse_term('x', 'x'), 'x'))
-print(print_term(parse_term('12', 'x'), 'x'))
-print(print_term(parse_term('1', 'x'), 'x'))
-print(print_term(parse_term('', 'x'), 'x'))
 
-  
-# 
-# 
-
-#     
-# 
-# 
-# # Second part is to have a function that implements the Power Rule
-# Don't need to know differentiator.  The parse functin takes care of giving back f'(x) wrt to x
 def apply_power_rule(equation_term):
     """ Applies the Differential Calculus Power Rule to an algebraic term, i.e. a*x^n = (n*a)x^n-1.
      
     Args:
-        equation_term (tuple)    The algebraic term (single equation element) for which the derivative is to be calculated.
-        differential (str)     The variable which derivative will be calculated for
+        equation_term        Tuple containing the (coefficient, exponent) of the term.
          
     Returns:
-        str                    The first derivative of term with respect to the differential
-    """
-   
-#     calculated_derivative = [0,0]
-#     if power > 0:
-#         calculated_derivative = [value*power, power - 1]
-# 
-#     return calculated_derivative 
+        tuple                The first derivative values of term.  Returns None if equation_term is invalid.
+    """   
     if equation_term == () or equation_term == None:
         return None
    
     coefficient, exponent = equation_term
     
-    # If the exponent is positive, then this will be 
+    # If the exponent is positive, then apply the power rule.  
     if exponent > 0:
         return (coefficient * exponent, exponent - 1)
     else:
         return (0,0)
 
-print ("\n\nPower rule tests ...")
-
-print(apply_power_rule(parse_term("16x", 'x')))
-print(apply_power_rule(parse_term("3x^2", 'x')))
-print(apply_power_rule(parse_term('x', 'x')))
-print(apply_power_rule(parse_term('12', 'x')))
-print(apply_power_rule(parse_term('', 'x')))
-
-
-
-# # Now for some differentiation and application of rules
-# 
-# # In[11]:
-# 
-# 
-# def differentiate_term(equation_term, differential):
-#     
-#     """ Calculates the first derivative of an algebraic term, e.g. f'(5x^2) = 10x.
-#     
-#     This function currently applies the Power Rule only.
-#     Future extension will be added to handle the Chain Rule also.
-#     
-#     Args:
-#         equation_term (str)    The algebraic term (single equation element) for which the derivative is to be calculated.
-#         differential (str)     The variable which derivative will be calculated for
-#         
-#     Returns:
-#         str                    The first derivative of term with respect to the differential
-#     """
-#     
-#     # If the differential is not here, this resolves to a constant which is always 0
-#     if differential not in equation_term:
-#         return ("0")
-#     
-#     diff_coeff, diff_exp = apply_power_rule (equation_term, differential)
-#     
-#     diff_term = ''
-#     
-#     if diff_exp == 0:
-#         diff_term = "{0}".format(diff_coeff)
-#     if diff_exp == 1:
-#         diff_term = "{0}{1}".format(diff_coeff, differential)
-#     else:
-#         diff_term = "{0}{1}^{2}".format(diff_coeff, differential, diff_exp)
-#     
-#     return diff_term
-# 
-# 
-# # In[12]:
 def differentiate_polynomial (equation, differential):
      
     """ Calculates the first derivative of a simple polynomial equation. 
@@ -248,16 +144,16 @@ def differentiate_polynomial (equation, differential):
      
     
     Args:
-        equation (str)    An algebraic equation for which the derivative is to be calculated.
+        equation (str)         An algebraic equation for which the derivative is to be calculated.
         differential (str)     The variable which derivative will be calculated for
          
     Returns:
         str                    The first derivative of the equation with respect to the differential
     """
-     
+    
     operators = re.compile("(\*|\/|\+|\-)")
        
-    # Iterate over the equation
+    # Split the equation, returning terms and operators
     equation_terms = re.split(operators, equation)
      
     diff_equation = ''
@@ -266,24 +162,74 @@ def differentiate_polynomial (equation, differential):
         if re.match(operators, term):
             diff_equation += f' {term} '
         else:
+            # Broken out for clarity
             src_term = parse_term(term.strip(), differential)
-            diff_equation += print_term(apply_power_rule(src_term), differential)
+            diff_term = apply_power_rule(src_term)
+            diff_equation += print_term(diff_term, differential)
      
     return diff_equation
 
-print ("\n\nDifferentiation tests ...")
+class TestPolynomialEtivity2(unittest.TestCase):
+    
+    def test_parse_term_simple_coefficient(self):
+        # Checks that the parsing is correct for simple coefficient
+        expected = (16,1)
+        result = parse_term("16x", 'x')
+        self.assertEqual(result, expected)
+        
+    def test_parse_term_basic(self):
+        # Checks that parsing is correct for basic, individual term (no explicit power or coefficient)
+        expected = (1,1)
+        result = parse_term("x", 'x')
+        self.assertEqual(result, expected)
+        
+    def test_parse_term_constant(self):
+        # Checks that parsing is correct for constants
+        expected = (12, 0)
+        result = parse_term("12", 'x')
+        self.assertEqual(result, expected)
+        
+    def test_parse_term_with_power(self):
+        # Checks that terms with power are handled
+        expected = (1,2)
+        result = parse_term("x^2", 'x')
+        self.assertEqual(result, expected)
+        
+    def test_parse_term_with_coefficient_and_power(self):
+        # Checks that terms with both a coefficient and power are handled
+        expected = (8,2)
+        result = parse_term("8x^2", 'x')
+        self.assertEqual(result, expected)
+        
+    def test_parse_term_empty(self):
+        expected = None
+        result = parse_term("", "")
+        self.assertEqual(result, expected)
+        
+    def test_power_rule_simple(self):
+        expected = (24,3)
+        result = apply_power_rule((6,4))
+        self.assertEqual(result, expected)
+        
+    def test_power_rule_constant(self):
+        expected = (0,0)
+        result = apply_power_rule((6,0))
+        self.assertEqual(result, expected)
 
-print(differentiate_polynomial("16x", 'x'))
-print(differentiate_polynomial("3x^2", 'x'))
-print(differentiate_polynomial("x", 'x'))
-print(differentiate_polynomial("12", 'x'))
-print(differentiate_polynomial("", 'x'))
-# 
-# 
-print(differentiate_polynomial ("x^501 + 3x^7 - 0.5x^6 + x^5 + 2x^3 + 3x^2 - 1", 'x'))
-print(differentiate_polynomial ("3x^2", 'x'))
-# print(differentiate_polynomial ("ax^3 + 0.5x^8", 'x'))
-# print(differentiate_polynomial ("5ax^3 + 0.5x^7 - 4bx^2", 'x'))
-print(differentiate_polynomial ("6x", 'x'))
-print(differentiate_polynomial ("16", 'x'))
-print(differentiate_polynomial('0', 'x'))
+    def test_etivity_fx(self):
+        expected = "6x"
+        result = differentiate_polynomial("3x^2", 'x')
+        self.assertEqual(result, expected)
+
+    def test_power_rule_gx(self):
+        expected = "2x + 16 + 0"
+        result = differentiate_polynomial("x^2 + 16x + 64", 'x')
+        self.assertEqual(result, expected)
+
+    def test_power_rule_kx(self):
+        expected = "501x^500 + 21x^6 - 3x^5 + 5x^4 + 6x^2 + 6x - 0"
+        result = differentiate_polynomial("x^501 + 3x^7 - 0.5x^6 + x^5 + 2x^3 + 3x^2 - 1", 'x')
+        self.assertEqual(result, expected)
+
+if __name__ == '__main__':
+    unittest.main(argv=[''], exit=False, verbosity=2)
